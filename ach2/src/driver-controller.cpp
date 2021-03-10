fdfac0e2c7ba353174d4f44e005dc34e7addf766
maisvendoo <maisvendoo@gmail.com>
2021-03-10 09:08:03 +0300

    Определены структуры данный для контроллера машиниста (состояние контактов на всех позициях)

--- ach2/src/driver-controller.cpp
+++ ach2/src/driver-controller.cpp
@@ -7,7 +7,25 @@ DriverController::DriverController(QObject *parent) : Device (parent)
   , main_pos(0)
   , revers_pos(0)
 {
+    // Коммутационная программа главного вала контроллера машиниста
+    // контакт             1  2  3  4  5  6  7  8  9  10
+    JK_contacts[POS_0] = { 0, 0, 1, 0, 0, 1, 0, 0, 0, 0 };
+    JK_contacts[POS_1] = { 0, 0, 1, 0, 0, 0, 0, 1, 0, 0 };
+    JK_contacts[POS_2] = { 0, 0, 0, 1, 0, 0, 0, 0, 0, 0 };
+    JK_contacts[POS_3] = { 1, 1, 0, 1, 0, 0, 1, 0, 0, 0 };
+    JK_contacts[POS_4] = { 1, 1, 0, 1, 0, 0, 0, 0, 0, 0 };
+    JK_contacts[POS_5] = { 1, 1, 0, 0, 1, 0, 0, 0, 0, 0 };
+    JK_contacts[POS_6] = { 1, 1, 0, 0, 1, 0, 0, 0, 0, 0 };
+    JK_contacts[POS_7] = { 1, 1, 0, 1, 1, 0, 0, 0, 0, 0 };
+    JK_contacts[POS_8] = { 1, 1, 0, 1, 1, 0, 0, 0, 0, 0 };
 
+    // Коммутационная программа реверсивного вала контроллера машиниста
+    // контакт                  1  2  3  4  5  6  7  8  9  10
+    JR_contacts[REV_POS_FWD] = {0, 0, 1, 0, 0, 1, 0, 0, 0, 0};
+    JR_contacts[REV_POS_FRT] = {1, 1, 1, 0, 0, 0, 0, 0, 0, 0};
+    JR_contacts[REV_POS_ZER] = {0, 0, 0, 0, 1, 0, 0, 0, 0, 0};
+    JR_contacts[REV_POS_BRT] = {1, 1, 0, 1, 0, 0, 0, 0, 0, 0};
+    JR_contacts[REV_POS_BWD] = {0, 0, 0, 1, 0, 1, 0, 0, 0, 0};
 }
 
 //------------------------------------------------------------------------------
@@ -16,6 +34,32 @@ DriverController::DriverController(QObject *parent) : Device (parent)
 DriverController::~DriverController()
 {
 
+}
+
+//------------------------------------------------------------------------------
+//
+//------------------------------------------------------------------------------
+bool DriverController::getJKstate(size_t contact_index) const
+{
+    if (contact_index < NUMBER_CONTACTS)
+    {
+        return static_cast<bool>(JK_contacts[static_cast<size_t>(main_pos)][contact_index]);
+    }
+
+    return false;
+}
+
+//------------------------------------------------------------------------------
+//
+//------------------------------------------------------------------------------
+bool DriverController::getJRstate(size_t contact_index) const
+{
+    if (contact_index < NUMBER_CONTACTS)
+    {
+        return static_cast<bool>(JR_contacts[static_cast<size_t>(revers_pos)][contact_index]);
+    }
+
+    return false;
 }
 
 //------------------------------------------------------------------------------
